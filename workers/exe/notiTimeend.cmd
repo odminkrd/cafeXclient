410ad0300a9fa14682688806b53aeb6bd93eeb30
odminkrd <odminkrd@gmail.com>
2024-01-24 11:05:43 +0300

    24-02-2024

--- workers/exe/notiTimeend.cmd
+++ workers/exe/notiTimeend.cmd
@@ -1,3 +1,3 @@
 chcp 65001
-nircmd.exe trayballoon "Внимание" "осталось не более 3 минут!" "shell32.dll,28" 5000
+nircmd.exe trayballoon "Внимание" "осталось не более 3 минут!" "shell32.dll,28" 2000
 msg * "осталось не более 3 минут!"
--- workers/exe/notiTimeendSec.cmd
+++ workers/exe/notiTimeendSec.cmd
@@ -1,3 +1,3 @@
 chcp 65001
-nircmd.exe trayballoon "Внимание" "осталось менее 1 минуты!" "shell32.dll,28" 5000
+nircmd.exe trayballoon "Внимание" "осталось менее 1 минуты!" "shell32.dll,28" 2000
 msg * "осталось менее 1 минуты!"
